e51703ec9b7a481b8c676c4094cdd94d883d5484
Saurabh Bahadur <133571086+saurabhbahadur@users.noreply.github.com>
2024-12-12 17:35:20 +0530

    Product added from mighty

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
-public class Main {
+class Product{
+    public int product(int x, int y){
+        return (x*y);
+    }
 
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+
+    public double product(double x , double y){
+        return x*y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(3,4));
+        System.out.println(p.product(3,4 , 7));
+        System.out.println(p.product(3.0,4.0));
+    }
 }
